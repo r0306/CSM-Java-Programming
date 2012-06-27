915e1d6de0e84807c3c592ff3471bc63196dbe15
Robert Lu <robertlu99@yahoo.com>
2012-06-26 20:55:41 -0700

    Lab 1 Uploaded

---
+++ src/Lab1/G00926921_Lab1.java
@@ -0,0 +1,25 @@
+package Lab1;
+
+/**
+* @Author: Robert Lu
+* @Email: robertlu99@yahoo.com
+* @Course: CIS 254
+* @Class: G00926921_Lab1
+* @Purpose: This is a basic class to print a self-introductory message to the console.
+*           Output should be: "Hello, my name is Robert Lu. I am currently a high school student."
+* @Usage: Hit the run button in the IDE or type 'java G00926921_Lab1' in command prompt.
+* @Lab Number: 1
+* @Date: 6/21/2012
+*/
+
+public class G00926921_Lab1 
+{
+	
+	public static void main (String args[])
+	{
+		
+		System.out.println("Hello, my name is Robert Lu. I am currently a high school student.");
+
+	}
+	
+}
